6f51196a2be2b8160ead87db42fe947a6482edd5
5alidev <khalidbenhamou.dev@outlook.com>
2024-07-31 21:44:06 +0000

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -60,3 +60,15 @@ function executeWork(employee: Director | Teacher): void {
     workTeacherTasks();
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
